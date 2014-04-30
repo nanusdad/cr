d7bd113e845b7cc52df11a35e2122fb9100ecdc7
nanusdad <arun@zechariah.net>
2014-04-30 11:19:46 -0500

    function fix in file2

--- file2.js
+++ file2.js
@@ -1,7 +1,7 @@
 var file2 = {};
 var ocr = require('./ocr.js');
 
-file2.ocrize(url, function(res) {
+file2.ocrize = function(url, function(res) {
 	ocr.run(url, function(text){
 		return text;
 	})
