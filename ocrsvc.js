e77983c39edbf208663d31da6a7160c9a6ebbd31
nanusdad <arun@zechariah.net>
2014-04-30 09:58:09 -0500

    set view dirname

--- ocrsvc.js
+++ ocrsvc.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 app.set('view engine', 'jade');
+app.set('views', __dirname);
 
 var jsonBody = require("body/json");
 var ocr = require('./ocr.js');
