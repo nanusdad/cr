84d986b17eed1b3f76e29d09b56f3489cc11ca57
nanusdad <arun@zechariah.net>
2014-05-01 09:31:09 -0500

    export functions in file2

--- file2.js
+++ file2.js
@@ -1,27 +1,19 @@
 var file2 = {};
 var ocr = require('./ocr.js');
 
-Object.getOwnPropertyNames(ocr).forEach(function(el) {
+var func_names = Object.getOwnPropertyNames(ocr).filter(function(property) {
+	return typeof ocr[property] === 'function';
+});
 
+func_names.forEach(function(el) {
 	file2[el] = function(url, lang, psm, callback) {
-		console.log(url +  ':' + lang + ':', psm);
-		ocr[el](url, lang, psm, function(text, err){
+		console.log(url + ':' + lang + ':', psm);
+		ocr[el](url, lang, psm, function(text, err) {
 			if (typeof callback === "function") {
 				callback(text, err);
 			}
 		});
 	};
 });
-
-// file2.ocrize = function(url, lang, psm, callback) {
-// 	console.log(url +  ':' + lang + ':', psm);
-// 	ocr.run(url, lang, psm, function(text, err){
-// 		if (typeof callback === "function") {
-// 			callback(text, err);
-// 		}
-// 	});
-// };
-
-// file2.ocr = file2.ocrize;
 
 module.exports = file2;
